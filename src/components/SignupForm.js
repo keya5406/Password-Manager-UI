b5f04400ce34f7294e13e5a92b5ce2e619b6fb61
keya5406 <139563713+keya5406@users.noreply.github.com>
2024-10-21 00:57:41 +0530

    Implemented user signup with PBKDF2 password hashing and salt generation

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
+import {useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
+    const navigate = useNavigate(); 
 
     const [email, setEmail] = useState('');
     const [masterPassword, setMasterPassword] = useState('');
@@ -14,7 +17,16 @@ const SignupForm = () => {
 
     const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+    const generateSalt = () => { 
+        return CryptoJS.lib.WordArray.random(128 / 8).toString();
+    };
 
+    const hashPassword = (password, salt) => {
+        return CryptoJS.PBKDF2(password, salt, {
+            keySize: 256 / 32,
+            iterations: 1000,
+        }).toString();
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -35,9 +47,15 @@ const SignupForm = () => {
             setError(newErrors);
             return;
         }
+        const salt = generateSalt(); 
+        const hashedPassword = hashPassword(masterPassword, salt); 
 
         console.log('Email:', email);
-        console.log('Master password;', masterPassword);
+        console.log('Generated Salt:', salt); 
+        console.log("Password:", masterPassword);
+        console.log('Hashed Password:', hashedPassword);
+
+        navigate('/CredentialForm');
 
         setEmail('');
         setMasterPassword('');
@@ -46,7 +64,7 @@ const SignupForm = () => {
     return (
         <form
             onSubmit={handleSubmit}
-            className="max-w-md mx-auto p-6 bg-blue-50flex-grow w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto p-4 md:p-6 bg-blue-50 rounded-lg shadow-lg mt-20 space-y-0"
+            className="max-w-md mx-auto p-6 bg-blue-50 flex-grow w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto p-4 md:p-6 bg-blue-50 rounded-lg shadow-lg mt-20 space-y-0"
         >
             <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
 
