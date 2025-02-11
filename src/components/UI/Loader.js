82618fab9674e7a973f84dad9ae2ddf621f6e7fa
keya5406 <139563713+keya5406@users.noreply.github.com>
2025-02-11 19:47:09 +0530

    Update documentation file

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -1,9 +1,7 @@
-import { FadeLoader } from 'react-spinners';
-
-const Loader = ({ color = '#3498db', size = 30 }) => {
+const Loader = () => {
   return (
-    <div className="flex justify-center items-center">
-      <FadeLoader color={color} size={size} />
+    <div className="flex justify-center items-center mt-20">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
     </div>
   );
 };
